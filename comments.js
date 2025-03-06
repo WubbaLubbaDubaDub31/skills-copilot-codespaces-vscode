0c5ea2d73dd47cf64fcc563be2c65e0fb7060bb9
WubbaLubbaDubaDub31 <144858544+WubbaLubbaDubaDub31@users.noreply.github.com>
2025-03-06 13:56:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,68 @@
 //Creat web server
-const express = require('express');
-const app = express();
-const port = 3000;
-//Create a route that listens to the root of the server
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-//Create a route that listens to the /comments path of the server
-app.get('/comments', (req, res) => {
-    res.send('This is the comments path');
-});
-//Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
+var path = require('path');
+var mime = require('mime');
+var comments = [];
+//Create server
+http.createServer(function(req, res){
+    //parse url
+    var urlObj = url.parse(req.url, true);
+    var pathname = urlObj.pathname;
+    //deal with different request
+    if(pathname == '/'){
+        fs.readFile('./index.html', function(err, data){
+            if(err){
+                console.log(err);
+                res.end('404 Not Found');
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    }else if(pathname == '/post'){
+        fs.readFile('./post.html', function(err, data){
+            if(err){
+                console.log(err);
+                res.end('404 Not Found');
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    }else if(pathname.indexOf('/public/') === 0){
+        fs.readFile('.' + pathname, function(err, data){
+            if(err){
+                console.log(err);
+                res.end('404 Not Found');
+            }else{
+                res.writeHead(200, {'Content-Type': mime.lookup(pathname)});
+                res.end(data);
+            }
+        });
+    }else if(pathname == '/comment'){
+        var comment = urlObj.query;
+        comment.dateTime = new Date();
+        comments.push(comment);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+    }else if(pathname == '/getComments'){
+        var str = JSON.stringify(comments);
+        res.end(str);
+    }else{
+        fs.readFile('.' + pathname, function(err, data){
+            if(err){
+                console.log(err);
+                res.end('404 Not Found');
+            }else{
+                res.writeHead(200, {'Content-Type': mime.lookup(pathname)});
+                res.end(data);
+            }
+        });
+    }
+}).listen(3000, function(){
+    console.log('running...');
 });
